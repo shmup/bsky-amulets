e31ca3bb0a59583df64b43e120c8de18753efc92
Jared Miller <jared@smell.flowers>
2024-12-03 16:17:16 -0500

    Add a tui bar to show progress

--- main.go
+++ main.go
@@ -5,52 +5,50 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
+	"time"
 
+	tm "github.com/buger/goterm"
 	"github.com/joho/godotenv"
-
 	amulet "github.com/shmup/amulet.go"
 	bsky "github.com/shmup/bluesky-firehose.go"
-
-	"github.com/bluesky-social/indigo/api/atproto"
 )
 
 func main() {
+	startTime := time.Now()
+	var postCount int
+	var amuletCount int
+
 	firehose, err := bsky.New("wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos")
 	if err != nil {
 		log.Fatal(err)
 	}
 	godotenv.Load()
 
-	email := os.Getenv("BSKY_EMAIL")
-	password := os.Getenv("BSKY_PASSWORD")
-
-	err = firehose.Authenticate(email, password)
-	if err != nil {
+	if err := firehose.Authenticate(os.Getenv("BSKY_EMAIL"), os.Getenv("BSKY_PASSWORD")); err != nil {
 		log.Fatal(err)
 	}
 
-	handler := &PostHandler{firehose: firehose}
-	firehose.Subscribe(context.Background(), handler)
-}
-
-type PostHandler struct {
-	firehose *bsky.Firehose
-}
-
-func (h *PostHandler) HandleEvent(evt *atproto.SyncSubscribeRepos_Commit) error {
-	for _, op := range evt.Ops {
-		if op.Action == "create" && strings.HasPrefix(op.Path, "app.bsky.feed.post") {
-			uri := fmt.Sprintf("at://%s/app.bsky.feed.post/%s",
-				evt.Repo, strings.TrimPrefix(op.Path, "app.bsky.feed.post/"))
-			if text, err := h.firehose.FetchPost(uri); err == nil {
-				isAmulet, rarity := amulet.IsAmulet(text)
-				if isAmulet {
-					fmt.Printf("%s", text)
-					fmt.Printf("Is Amulet: %t Rarity: %d\n", isAmulet, rarity)
-				}
-			}
+	// Update status line every second
+	go func() {
+		for {
+			tm.Clear()
+			tm.MoveCursor(1, 1)
+			tm.Printf("Posts Processed: %d | Amulets Found: %d | Running for: %s\n",
+				postCount,
+				amuletCount,
+				time.Since(startTime).Round(time.Second))
+			tm.Flush()
+			time.Sleep(time.Second)
 		}
-	}
-	return nil
+	}()
+
+	firehose.OnPost(context.Background(), func(text string) error {
+		postCount++
+		if isAmulet, rarity := amulet.IsAmulet(text); isAmulet {
+			amuletCount++
+			fmt.Printf("\n%s", text)
+			fmt.Printf("Is Amulet: %t Rarity: %d\n", isAmulet, rarity)
+		}
+		return nil
+	})
 }
